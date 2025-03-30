d5b5bb087a2a8c2a1dd706843a97a230850d8150
Konstantin Seregin <kraken114@bk.ru>
2025-03-30 20:22:58 +0300

    добавление ноды DetectionTrackingNodes.py

--- elements/FrameElement.py
+++ elements/FrameElement.py
@@ -10,8 +10,34 @@ class FrameElement:
         frame: np.ndarray,
         frame_num: float,
         frame_result: np.ndarray | None = None,
+        # detection
+        detected_conf: list | None = None,
+        detected_cls: list | None = None,
+        detected_xyxy: list[list] | None = None,
+        tracked_conf: list | None = None,
+        tracked_cls: list | None = None,
+        tracked_xyxy: list[list] | None = None,
+        id_list: list | None = None,
+        # далее
+
+
+
+
+
+
     ) -> None:
         self.source = source  # Путь к видео или номер камеры с которой берем поток
         self.frame = frame  # Кадр bgr формата
         self.frame_num = frame_num  # Нормер кадра с потока
         self.frame_result = frame_result  # Итоговый обработанный кадр
+        # ------detection-------
+        # Список уверенностей задетектированных объектов
+        self.detected_conf = detected_conf
+        self.detected_cls = detected_cls  # Список классов задетектированных объектов
+        self.detected_xyxy = detected_xyxy  # Список списков с координатами xyxy боксов
+        # Результаты корректировки трекинг алгоритмом:
+        self.tracked_conf = tracked_conf  # Список уверенностей задетектированных объектов
+        self.tracked_cls = tracked_cls  # Список классов задетектированных объектов
+        self.tracked_xyxy = tracked_xyxy  # Список списков с координатами xyxy боксов
+        self.id_list = id_list  # Список обнаруженных id трекуемых объектов
+        # -----------------------
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import hydra
 from nodes.VideoReader import VideoReader
+from nodes.DetectionTrackingNodes import DetectionTrackingNodes
 import cv2
 
 
 @hydra.main(version_base=None, config_path="conf", config_name="config")
 def main(config) -> None:
     video_reader = VideoReader(config["video_reader"])
+    detection_node = DetectionTrackingNodes(config)
 
     for frame_element in video_reader.process():
+        frame_element = detection_node.process(frame_element)
         print('iiiiiiiiii')
         print()
 
